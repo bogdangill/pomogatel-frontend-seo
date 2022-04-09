c0a621d71fdefb97b8cbe73f1578934e78cfbb90
bogdangill <herecomesthefog@gmail.com>
2022-04-09 08:29:28 +0500

    upgrade dropdown module

--- #src/modules/dropdown/dropdown.js
+++ #src/modules/dropdown/dropdown.js
@@ -8,6 +8,16 @@ for (let dropdown of dropdowns) {
         ddBody.classList.add('dropdown-body--visible');
     });
 
+    let ddItems = ddBody.querySelectorAll('a');
+    let defaultVal = dropdown.querySelector('.dropdown__value');
+
+    for (let ddItem of ddItems) {
+        ddItem.addEventListener('click', () => {
+            let selectedVal = ddItem.textContent.split(' и ')[0];
+            defaultVal.textContent = selectedVal;
+        })
+    }
+
     document.addEventListener('click', function(e) {
         if (!e.target.closest('.dropdown__value')) {
             ddBody.classList.remove('dropdown-body--visible');
