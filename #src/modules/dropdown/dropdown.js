716ebecd17501a25e6b9ca295b1059e1140dee16
bogdangill <herecomesthefog@gmail.com>
2022-04-10 17:44:26 +0500

    upgrade dropdown module, add arrowkey navigation

--- #src/modules/dropdown/dropdown.js
+++ #src/modules/dropdown/dropdown.js
@@ -2,13 +2,18 @@ let dropdowns = document.querySelectorAll('.dropdown');
 
 function toggleDropdown(dd, isOpened = false) {
     let ddBody = dd.querySelector('.dropdown-body');
+    let trigger = dd.querySelector('.dropdown__value');
 
     if (isOpened) {
         ddBody.classList.remove('dropdown-body--visible');
         ddBody.classList.add('dropdown-body--hidden');
+        trigger.setAttribute("aria-expanded", "false");
     } else {
         ddBody.classList.remove('dropdown-body--hidden');
         ddBody.classList.add('dropdown-body--visible');
+        trigger.setAttribute("aria-expanded", "true");
+        
+        ddBody.firstElementChild.firstElementChild.focus();
     }
 }
 
@@ -32,6 +37,24 @@ for (let dropdown of dropdowns) {
     });
 
     document.addEventListener('keydown', function(e) {
+        let dropdownSibling = document.activeElement.nextElementSibling;
+
+        if (e.key === 'ArrowUp') {
+            if (dropdownSibling) {
+                dropdownSibling.childNodes[0].previousElementSibling.firstElementChild.focus();
+            } else {
+                dropdownSibling = document.activeElement.parentElement.previousElementSibling.firstElementChild;
+                dropdownSibling.focus();
+            }
+        }
+        if (e.key == 'ArrowDown') {
+            if (dropdownSibling) {
+                dropdownSibling.childNodes[0].nextElementSibling.firstElementChild.focus();
+            } else {
+                dropdownSibling = document.activeElement.parentElement.nextElementSibling.firstElementChild;
+                dropdownSibling.focus();
+            }
+        }
         if (e.key === 'Escape') {
             toggleDropdown(dropdown, true);
         }
