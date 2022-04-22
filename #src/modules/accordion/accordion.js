670e810367e3f6a10a599e3f5e60beca08393ab5
bogdangill <herecomesthefog@gmail.com>
2022-04-22 18:18:37 +0500

    accordion refactoring (make better animation) pt.1

--- #src/modules/accordion/accordion.js
+++ #src/modules/accordion/accordion.js
@@ -12,6 +12,18 @@ function closeAcc(acc) {
 if (accordions.length > 0) {
     accordions.forEach(accordion => {
         accordion.addEventListener("click", () => {
+            let accBody = accordion.querySelector('.accordion__body');
+            let accBodyInnerHeight = 0;
+
+            if (accBody.hasChildNodes()) {
+                let children = accBody.childNodes;
+
+                for (let child of children) {
+                    //без проверки помимо высоты детей суммирует непонятно откуда взявшийся undefined и в итоге NaN. костыль.
+                    if (child.offsetHeight !== undefined) accBodyInnerHeight += child.offsetHeight;
+                }
+            }
+
             if (accordion.classList.contains('accordion--hidden')) {
                 accordions.forEach(acc => closeAcc(acc))
             } else {
@@ -19,6 +31,7 @@ if (accordions.length > 0) {
                 return
             }
 
+            accBody.style.height = accBodyInnerHeight + 'px';
             accordion.classList.remove('accordion--hidden');
             accordion.classList.add('accordion--visible');
             accordion.querySelector('.accordion__trigger').setAttribute('aria-expanded', 'true');
