a11d871b9f83e062e4c438e804316fc0269e77b2
bogdangill <herecomesthefog@gmail.com>
2022-04-22 12:59:57 +0500

    correct reviews

--- #src/sections/reviews/reviews.js
+++ #src/sections/reviews/reviews.js
@@ -1,9 +1,28 @@
-const swiper = new Swiper('.swiper-container', {
-    spaceBetween: 32,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    }
-});
+const swipers = document.querySelectorAll('.swiper-container')
 
-swiper.init();
+if (swipers.length) {
+    const swiper = new Swiper('.swiper-container', {
+        spaceBetween: 32,
+        slidesPerView: 'auto',
+        initialSlide: 1,
+        centeredSlides: true,
+        breakpoints: {
+            767: {
+                spaceBetween: 16,
+                slidesPerView: 3,
+                centeredSlides: false
+            },
+            1199: {
+                spaceBetween: 32,
+                slidesPerView: 3,
+                centeredSlides: false
+            }
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        }
+    });
+
+    swiper.init();
+}
